fde49ba59e95b263f9a13ae2edb3984bff3c6d09
rangelz1 <rangelz187@gmail.com>
2017-05-20 13:33:34 -0700

    query added to list.js

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -6,7 +6,32 @@
 
 
 		function ListController(){
-			
+			var vm = this;
+
+			vm.data = listOfQuestons;
 		}
+
+		var listOfQuestons = [
+			{
+				type: "Question One",
+				image_url: "http://lorempixel.com/400/200/",
+				location: "Position One",
+			},
+			{
+				type: "Question Two",
+				image_url: "http://lorempixel.com/400/200/",
+				location: "Position Two",
+			},
+			{
+				type: "Question Three",
+				image_url: "http://lorempixel.com/400/200/",
+				location: "Position Three",
+			},
+			{
+				type: "Question Four",
+				image_url: "http://lorempixel.com/400/200/",
+				location: "Position Four",
+			}
+		]
 
 })();
