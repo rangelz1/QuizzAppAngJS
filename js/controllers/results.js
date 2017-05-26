0e6e8896a3b0f1dcbe134b401b10347db36e4022
rangelz1 <rangelz187@gmail.com>
2017-05-26 12:47:03 -0700

    Quiz App is finished but it has a few bugs that need to be fixed, main one on the check answers after completed

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -13,6 +13,7 @@
 			vm.dataService = DataService;
 			vm.getAnswerClass = getAnswerClass;
 			vm.setActiveQuestion = setActiveQuestion;
+			vm.reset = reset;
 			vm.calculatePerc = calculatePerc; 
 			vm.activeQuestion = 0;
 
@@ -29,6 +30,18 @@
 					return "bg-success";
 				}else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
 					return "bg-danger";
+				}
+			}
+
+			function reset(){
+				quizMetrics.changeState("results", false);
+				quizMetrics.numCorrect = 0;
+
+				for(var i = 0; i < DataService.quizQuestions.length; i++) {
+					var data = DataService.quizQuestions[i];
+
+					data.selected = null;
+					data.correct = null;
 				}
 			}
 		}
